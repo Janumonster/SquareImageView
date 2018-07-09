e7977292a16f9007675deab15a53d14ea1688922
ZJ-DB1371\Administrator <525270921@qq.com>
2018-07-09 20:10:14 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/janumonster/squareimageview/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.janumonster.squareimageview;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented horizontal, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under horizontal.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.janumonster.squareimageview", appContext.getPackageName());
+    }
+}
